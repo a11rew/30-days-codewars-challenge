0db822f645c5d94c67745710f1b4260f237adc12
Andrew Glago <andrewglago1@gmail.com>
2022-01-16 12:56:25 +0000

    day 11

---
+++ day-11/solution.js
@@ -0,0 +1,12 @@
+function alphabetPosition(text) {
+  let alphabets = Array.from(Array(26))
+    .map((_, i) => i + 65)
+    .map((x) => String.fromCharCode(x));
+  let positions = [];
+
+  [...text].forEach((e) => {
+    if (alphabets.includes(e.toUpperCase()))
+      positions.push(alphabets.indexOf(e.toUpperCase()) + 1);
+  });
+  return positions.join(" ");
+}
